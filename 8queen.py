5e2353fb8374b8da65539682e01a57e993565a0a
Kishorebalaji670 <157690934+Kishorebalaji670@users.noreply.github.com>
2024-03-05 14:16:32 +0530

    Update 8queen.py

--- 8queen.py
+++ 8queen.py
@@ -1,45 +1,40 @@
-def is_safe(board, row, col):
-    for i in range(col):
-        if board[row][i] == 1:
-            return False
-    for i, j in zip(range(row, -1, -1), range(col, -1, -1)):
-        if board[i][j] == 1:
-            return False
-    for i, j in zip(range(row, N, 1), range(col, -1, -1)):
-        if board[i][j] == 1:
-            return False
-    return True
-def solve_n_queens(board, col):
-    if col >= N:
-        return True
-    for i in range(N):
-        if is_safe(board, i, col):
-            board[i][col] = 1
-            if solve_n_queens(board, col + 1) == True:
-                return True
-            board[i][col] = 0
-    return False
-def print_solution(board):
-    for i in range(N):
-        for j in range(N):
-            print(board[i][j], end=" ")
-        print()
-def solve_8_queens():
-    board = [[0 for _ in range(N)] for _ in range(N)]
-    if solve_n_queens(board, 0) == False:
-        print("Solution does not exist")
+def safe(board, row, col):
+    # Check if there is a queen in the same row
+    if any(board[row]):
         return False
-    print_solution(board)
-    return True
-N = 8
-solve_8_queens()
 
-OUTPUT:-
-1 0 0 0 0 0 0 0 
-0 0 0 0 0 0 1 0 
-0 0 0 0 1 0 0 0 
-0 0 0 0 0 0 0 1 
-0 1 0 0 0 0 0 0 
-0 0 0 1 0 0 0 0 
-0 0 0 0 0 1 0 0 
-0 0 1 0 0 0 0 0 
+    # Check if there is a queen in the same column
+    if any(board[i][col] for i in range(len(board))):
+        return False
+
+    # Check if there is a queen in the same diagonal (upper left to lower right)
+    if any(board[i][j] for i, j in zip(range(row, -1, -1), range(col, -1, -1))):
+        return False
+
+    # Check if there is a queen in the same diagonal (upper right to lower left)
+    if any(board[i][j] for i, j in zip(range(row, -1, -1), range(col, len(board)))):
+        return False
+
+    return True
+
+def print_board(board):
+    for row in board:
+        print(" ".join("Q" if cell else "." for cell in row))
+    print()
+
+def solve_queens(board, row):
+    if row == len(board):
+        print_board(board)
+        return
+
+    for col in range(len(board)):
+        if safe(board, row, col):
+            board[row][col] = 1
+            solve_queens(board, row + 1)
+            board[row][col] = 0  # Backtrack
+
+if __name__ == "__main__":
+    board_size = 8
+    chessboard = [[0] * board_size for _ in range(board_size)]
+
+    solve_queens(chessboard, 0)
